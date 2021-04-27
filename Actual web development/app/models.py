e4a889cee79cf2545654937e35ef5e3493d683cc
Dylan-peel <65403166+Dylan-peel@users.noreply.github.com>
2021-04-27 12:18:03 +0100

    Add files via upload

--- Actual web development/app/models.py
+++ Actual web development/app/models.py
@@ -1,18 +1,39 @@
 from app import db
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, current_user, login_user
 
-class User(db.Model):
+class account(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, index=True, unique=True)
-    password = db.Column(db.String, index=True, unique=True)
+    passwordhash = db.Column(db.String, index=True, unique=True)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.passwordhash, password)
+
+    def get_id(self):
+        return(id)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-class question(db.Model):
+    def __init__(self, username, password):
+        self.username = username
+        self.passwordhash = generate_password_hash(password)
+
+@login.user_loader
+def load_user(id):
+    return account.query.get(id)
+
+
+
+
+class Question(db.Model):
     QuestionID = db.Column(db.Integer, primary_key=True)
     Questionbody = db.Column(db.String(1000))
     Questionanswer = db.Column(db.String(500))
-    creatorid = db.Column(db.Integer, db.ForeignKey('id'))
+    creatorid = db.Column(db.Integer, db.ForeignKey('account.id'))
 
     def __repr__(self):
         return '<Question {}>'.format(self.body)
--- Actual web development/app/routes.py
+++ Actual web development/app/routes.py
@@ -1,31 +1,66 @@
-from flask import render_template, session, flash, redirect, request
-from time import sleep
+from flask import render_template, session, flash, redirect, request, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+
 from app import app
-from app.form import LoginForm, QuestionForm
+from app.form import LoginForm, QuestionForm, RegisterForm
+from app import db
+from app.models import account, Question
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world!"
+    return render_template('basepage.html', title='Index')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if request.method == 'POST':
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))#user will not load if they are already logged in
+        form = LoginForm()
         if form.validate_on_submit():
-            flash("Login successful!")
-            sleep(2)
-            return redirect('/index')
-        return render_template('login.html', title='Login', form=form)
-    else:
-        return render_template('login.html', title='Login', form=form)
+            user = account.query.filter_by(username=form.user.data).first()#searches through user DB for matching user
+            #if user is None or not user.check_password(form.passwd.data):#if user is not valid or password is incorrect
+                #flash("Invalid Username or password")
+                #return redirect(url_for(login))
+            if user.check_password(form.passwd.data) and user is not None:
+                print("hello world")
+                login_user(user)
+            else:
+                flash("Invalid Username or password")
+                return redirect(url_for(login))
+            return redirect(url_for('index'))#redirects back to index page after succesfull login
+        else:
+            return render_template('login.html', title='Login', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = account(username=form.user.data, password=form.passwd.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('New user has been registered')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Registration', form=form)
+
+
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for(index))
+
+
 
 @app.route('/question', methods=['GET', 'POST'])
 def question():
-    form = QuestionForm
+    form = QuestionForm() #These brackets were missing at the end of QuestionForm, which was causing the error
     if request.method == 'POST':
         if form.validate_on_submit():
             flash("Question Posted")
-            return redirect('/question')
+            return redirect(url_for('/question'))
         return render_template('question.html', form=form)
-
+    else:
+        return render_template('question.html', title='Questions', form=form)
