7498f717f31eb10fd84bc61db37b34abbf5e2637
Dylan-peel <65403166+Dylan-peel@users.noreply.github.com>
2021-05-14 14:12:19 +0100

    Add files via upload

--- app/form.py
+++ app/form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
+from app.models import account, Question
 
 class LoginForm(FlaskForm):
     user = StringField('Username', validators=[DataRequired()])
@@ -12,3 +13,14 @@ class QuestionForm(FlaskForm):
     Question = StringField('Question', validators=[DataRequired()])
     Answer = StringField('Answer', validators=[DataRequired()])
     sub = SubmitField('Post question')
+
+class RegisterForm(FlaskForm):
+    user = StringField('Username', validators=[DataRequired()])
+    passwd = PasswordField('Password', validators=[DataRequired()])
+    repeatpass = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('passwd')])
+    sub = SubmitField('sign in')
+
+    def uservalidation(self, user):
+        user = account.query.filter_by(username=user.data)
+        if user is not None:
+            raise ValidationError('Please enter a different username as this has been used previously')
--- app/routes.py
+++ app/routes.py
@@ -2,48 +2,65 @@ from flask import render_template, session, flash, redirect, request, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 
 from app import app
-from app.form import LoginForm, QuestionForm
+from app.form import LoginForm, QuestionForm, RegisterForm
 from app import db
-from app.models import User, Question
+from app.models import account, Question
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world!"
+    return render_template('basepage.html', title='Index')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))#user will not load if they are already logged in
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = account.query.filter_by(username=form.user.data).first()#searches through user DB for matching user
+            #if user is None or not user.check_password(form.passwd.data):#if user is not valid or password is incorrect
+                #flash("Invalid Username or password")
+                #return redirect(url_for(login))
+            if user.check_password(form.passwd.data) and user is not None:
+                print("Database lookup success")
+                login_user(user)
+            else:
+                flash("Invalid Username or password")
+                return redirect(url_for(login))
+            return redirect(url_for('index'))#redirects back to index page after succesfull login
+        else:
+            return render_template('login.html', title='Login', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    form = LoginForm()
+    form = RegisterForm()
     if form.validate_on_submit():
-        user=User.query.filter_by(username=form.user.data).first()
-        if user is None or not user.check_password(form.passwd.data):
-            flash("Invalid Username or password")
-            return redirect(url_for(login))
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for(index))
-    else:
-        return render_template('login.html', title='Login', form=form)
+        user = account(username=form.user.data, password=form.passwd.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('New user has been registered')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Registration', form=form)
+
+
+
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for(index))
 
-@app.route('/register')
-def register():
-
 
 
 @app.route('/question', methods=['GET', 'POST'])
-@login_required
 def question():
-    form = QuestionForm
+    form = QuestionForm() #These brackets were missing at the end of QuestionForm, which was causing the error
     if request.method == 'POST':
         if form.validate_on_submit():
             flash("Question Posted")
             return redirect(url_for('/question'))
         return render_template('question.html', form=form)
     else:
-        data=Question.query.filter_by(creatorid=User.get_id())
+        return render_template('question.html', title='Questions', form=form)
